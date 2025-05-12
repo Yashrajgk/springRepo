1fb37e8b614b6670929d1dc9d7f3eedd1441a49d
Yashraj Kanake <yashrajkanake2002@gmail.com>
2025-05-12 12:12:57 +0530

    second commit

--- src/main/java/com/capgemini/helloWorld/Main.java
+++ src/main/java/com/capgemini/helloWorld/Main.java
@@ -3,5 +3,6 @@ package com.capgemini.helloWorld;
 public class Main {
 	public static void main(String args) {
 		System.out.println("Hello ygkk");
+		System.out.println("I am duniya ka dad");
 	}
 }
